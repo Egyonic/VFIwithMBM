1037899cbc261d2a91fd85e44e2cfbfe449d3f15
Egyonic <egyonic@gmail.com>
2024-07-31 17:03:16 +0800

    Restormer_EffcientVit

--- model/refine_tf.py
+++ model/refine_tf.py
@@ -11,6 +11,7 @@ import numbers
 
 from einops import rearrange
 from model.biformer_models.biformer import Block as BiformerBlock
+from model.efficientvit.models.nn.ops import EfficientViTBlock
 
 
 ##########################################################################
@@ -421,10 +422,110 @@ class Restormer_BRA(nn.Module):
         return torch.sigmoid(out_dec_level1)
 
 
+class Restormer_EffcientVit(nn.Module):
+    def __init__(self,
+                 inp_channels=49,
+                 out_channels=3,
+                 dim=48,
+                 num_blocks=[2, 4, 4, 2],
+                 num_refinement_blocks=2,
+                 heads=[1, 2, 4, 8],
+                 ffn_expansion_factor=1,
+                 LayerNorm_type='WithBias',  ## Other option 'BiasFree'
+                 bias=False,
+                 ):
+        super(Restormer_EffcientVit, self).__init__()
+
+        self.patch_embed = OverlapPatchEmbed(inp_channels, dim)
+
+        self.encoder_level1 = nn.Sequential(*[
+            TransformerBlock(dim=dim, num_heads=heads[0], ffn_expansion_factor=ffn_expansion_factor, bias=bias,
+                             LayerNorm_type=LayerNorm_type) for i in range(num_blocks[0])])
+
+        self.c_down1 = nn.Conv2d(64 + 1, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.down1_2 = Downsample(dim, dim)  ## From Level 1 to Level 2
+        self.encoder_level2 = nn.Sequential(*[
+            TransformerBlock(dim=int(dim * 2 ** 1 + 32), num_heads=heads[1], ffn_expansion_factor=ffn_expansion_factor,
+                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_blocks[1])])
+
+        self.c_down2 = nn.Conv2d(128 + 1, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.down2_3 = Downsample(dim * 2 + 32, dim * 2)  ## From Level 2 to Level 3
+        self.encoder_level3 = nn.Sequential(*[
+            EfficientViTBlock(in_channels=dim * 4 + 64, dim=dim, heads_ratio=1.0, expand_ratio=2.0)
+            for i in range(num_blocks[2])])
+
+        self.c_down3 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.down3_4 = Downsample(int(dim * 4 + 64), dim * 4)  ## From Level 3 to Level 4
+        self.latent = nn.Sequential(*[
+            EfficientViTBlock(in_channels=dim * 8 + 128, dim=dim, heads_ratio=1.0, expand_ratio=2.0)
+            for i in range(num_blocks[3])])
+
+        self.up4_3 = Upsample(dim * 8 + 128)  ## From Level 4 to Level 3
+        self.reduce_chan_level3 = nn.Conv2d(dim * 8 + 128, dim * 4 + 64, kernel_size=1, bias=bias)
+        self.decoder_level3 = nn.Sequential(*[
+            EfficientViTBlock(in_channels=dim * 4 + 64, dim=dim, heads_ratio=1.0, expand_ratio=2.0)
+            for i in range(num_blocks[2])])
+
+        self.up3_2 = Upsample(dim * 4 + 64)  ## From Level 3 to Level 2
+        self.reduce_chan_level2 = nn.Conv2d(dim * 4 + 64, dim * 2 + 32, kernel_size=1, bias=bias)
+        self.decoder_level2 = nn.Sequential(*[
+            TransformerBlock(dim=int(dim * 2 + 32), num_heads=heads[1], ffn_expansion_factor=ffn_expansion_factor,
+                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_blocks[1])])
+
+        self.up2_1 = Upsample(dim * 2 + 32)  ## From Level 2 to Level 1  (NO 1x1 conv to reduce channels)
+
+        self.decoder_level1 = nn.Sequential(*[
+            TransformerBlock(dim=int(dim * 1 + 16 + dim), num_heads=heads[0], ffn_expansion_factor=ffn_expansion_factor,
+                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_blocks[0])])
+
+        self.refinement = nn.Sequential(*[
+            TransformerBlock(dim=int(dim * 1 + 16 + dim), num_heads=heads[0], ffn_expansion_factor=ffn_expansion_factor,
+                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_refinement_blocks)])
+
+        self.output = nn.Conv2d(dim * 1 + 16 + dim, out_channels, kernel_size=3, stride=1, padding=1, bias=bias)
+
+    def forward(self, img0, img1, warped_img0, warped_img1, mask, flow, c0, c1, mask_guide):
+        inp_img = torch.cat((img0, img1, mask, mask_guide[0], warped_img0, warped_img1, c0[0], c1[0], flow), 1)
+
+        inp_enc_level1 = self.patch_embed(inp_img)
+        out_enc_level1 = self.encoder_level1(inp_enc_level1)
+
+        f1 = self.c_down1(torch.cat([mask_guide[1], c0[1], c1[1]], 1))
+        inp_enc_level2 = self.down1_2(out_enc_level1)
+        out_enc_level2 = self.encoder_level2(torch.cat([inp_enc_level2, f1], 1))
+
+        f2 = self.c_down2(torch.cat([mask_guide[2], c0[2], c1[2]], 1))
+        inp_enc_level3 = self.down2_3(out_enc_level2)
+        out_enc_level3 = self.encoder_level3(torch.cat([inp_enc_level3, f2], 1))
+
+        f3 = self.c_down3(torch.cat([c0[3], c1[3]], 1))
+        inp_enc_level4 = self.down3_4(out_enc_level3)
+        latent = self.latent(torch.cat([inp_enc_level4, f3], 1))
+
+        inp_dec_level3 = self.up4_3(latent)
+        inp_dec_level3 = torch.cat([inp_dec_level3, out_enc_level3], 1)
+        inp_dec_level3 = self.reduce_chan_level3(inp_dec_level3)
+        out_dec_level3 = self.decoder_level3(inp_dec_level3)
+
+        inp_dec_level2 = self.up3_2(out_dec_level3)
+        inp_dec_level2 = torch.cat([inp_dec_level2, out_enc_level2], 1)
+        inp_dec_level2 = self.reduce_chan_level2(inp_dec_level2)
+        out_dec_level2 = self.decoder_level2(inp_dec_level2)
+
+        inp_dec_level1 = self.up2_1(out_dec_level2)
+        inp_dec_level1 = torch.cat([inp_dec_level1, out_enc_level1], 1)
+        out_dec_level1 = self.decoder_level1(inp_dec_level1)
+
+        out_dec_level1 = self.refinement(out_dec_level1)
+        out_dec_level1 = self.output(out_dec_level1)
+
+        return torch.sigmoid(out_dec_level1)
+
+
 
 if __name__ == "__main__":
     # flownet = IFNet_bf_resnet_local_mae()
-    flownet = Restormer_BRA(inp_channels=49,out_channels=3, dim=32)
+    flownet = Restormer_EffcientVit(inp_channels=49,out_channels=3, dim=32)
     input = torch.rand(1, 17, 224, 224)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     flownet.to(device)
